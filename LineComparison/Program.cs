fa1192bb2298d50dce67b5d3a8c9e5b57e260fd7
Wpaswan <dablumcapaswan@gmail.com>
2021-11-22 09:39:27 -0800

    Uc1 added

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Welcome to Line Comparison Program!");
+﻿
+using static System.Math;
+
+namespace LINECOMPARISION
+{
+    class Program
+
+    {
+        public static void Main(string[] args)
+        {
+            //user Input
+            Console.Write("Enter any  value x1: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter any  value x2: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter any  value y1: ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter any  value y2: ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+
+          double  distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            Console.WriteLine(distance);
+            Console.ReadKey();
+        }
+    }
+
+
+}
