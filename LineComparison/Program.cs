d7646f1ff6eec4fd9b78178b9990583acfdaeef7
Wpaswan <dablumcapaswan@gmail.com>
2021-11-22 09:52:12 -0800

    Uc2 of Line Comp

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -1,9 +1,11 @@
-﻿
+﻿using System;
+//using System.Math;
 using static System.Math;
 
-namespace LINECOMPARISION
+namespace LINE
+
 {
-    class Program
+    class Program1
 
     {
         public static void Main(string[] args)
@@ -18,9 +20,16 @@ namespace LINECOMPARISION
             Console.Write("Enter any  value y2: ");
             double y2 = Convert.ToDouble(Console.ReadLine());
 
+            if (x1 == x2 && y1 == y2)
+            {
+                Console.WriteLine("Two lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Two lines are not equal");
+            }
 
-          double  distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-            Console.WriteLine(distance);
+
             Console.ReadKey();
         }
     }
