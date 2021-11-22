bc962e986e4afa9c0b127f33edf45581dbb8f978
Wpaswan <dablumcapaswan@gmail.com>
2021-11-22 10:04:01 -0800

    Uc3 of Line Comparison added

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -1,13 +1,14 @@
-﻿using System;
-//using System.Math;
-using static System.Math;
+﻿using static System.Math;
 
-namespace LINE
-
+namespace LINECOMPARISION
 {
-    class Program1
+    class Program
 
     {
+
+
+        //user Input
+
         public static void Main(string[] args)
         {
             //user Input
@@ -20,19 +21,23 @@ namespace LINE
             Console.Write("Enter any  value y2: ");
             double y2 = Convert.ToDouble(Console.ReadLine());
 
-            if (x1 == x2 && y1 == y2)
-            {
-                Console.WriteLine("Two lines are equal");
-            }
+            double d1 = x1 - y1;
+            double d2 = x2 - y2;
+
+            int distance = d1.CompareTo(d2);
+
+            // checking the status
+            if (distance > 0)
+                Console.WriteLine("line 1 is greater than line 2");
+            else if (distance < 0)
+                Console.WriteLine("line 1 is less than line 2");
             else
-            {
-                Console.WriteLine("Two lines are not equal");
-            }
+                Console.WriteLine("line 1 is equal to line 2");
 
 
             Console.ReadKey();
         }
+
+
     }
-
-
 }
