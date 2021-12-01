51d31ecfafb0ce01258f5084309906c6a6b65fe8
Wpaswan <dablumcapaswan@gmail.com>
2021-11-30 21:29:35 -0800

    [Dablu modified Using Compare to method]

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -11,7 +11,7 @@ namespace LINECOMPARISION
 
         public static void Main(string[] args)
         {
-            //user Input
+            //Cordinates of first line
             Console.Write("Enter any  value x1: ");
             double x1 = Convert.ToDouble(Console.ReadLine());
             Console.Write("Enter any  value x2: ");
@@ -20,12 +20,22 @@ namespace LINECOMPARISION
             double y1 = Convert.ToDouble(Console.ReadLine());
             Console.Write("Enter any  value y2: ");
             double y2 = Convert.ToDouble(Console.ReadLine());
+            //Cordinates of second line
+            Console.Write("Enter any  value X1: ");
+            double X1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter any  value X2: ");
+            double X2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter any  value Y1: ");
+            double Y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter any  value Y2: ");
+            double Y2 = Convert.ToDouble(Console.ReadLine());
 
-            double d1 = x1 - y1;
-            double d2 = x2 - y2;
+            double d1 = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+            double d2 = Math.Sqrt((X2-X1)*(X2-X1)+(Y2-Y1)*(Y2-Y1));
 
             int distance = d1.CompareTo(d2);
-
+            Console.WriteLine("Length of first line:"+d1);
+            Console.WriteLine("Length of second line:"+d2);
             // checking the status
             if (distance > 0)
                 Console.WriteLine("line 1 is greater than line 2");
